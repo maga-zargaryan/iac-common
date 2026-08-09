2631b6ed132dd30427a27775d6c34dff73cc6453
Margarita Zargaryan <Margarita.Zargaryan@partner.bmw.de>
2026-08-10 00:38:40 +0400

    Add separate subnet for golden ami build.

---
+++ terraform/endpoints.tf
@@ -0,0 +1,63 @@
+resource "aws_security_group" "vpc_endpoints" {
+  name        = "${local.name}-vpce"
+  description = "Allows HTTPS to private endpoints from within the VPC"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "HTTPS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+
+  tags = {
+    Name = "${local.name}-vpce"
+  }
+}
+
+module "vpc_endpoints" {
+  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+  version = "5.13.0"
+
+  vpc_id             = module.vpc.vpc_id
+  security_group_ids = [aws_security_group.vpc_endpoints.id]
+
+  endpoints = {
+    s3 = {
+      service         = "s3"
+      service_type    = "Gateway"
+      route_table_ids = concat(
+        module.vpc.private_route_table_ids,
+        module.vpc.database_route_table_ids,
+      )
+      tags = { Name = "${local.name}-vpce-s3" }
+    }
+
+    ssm = {
+      service             = "ssm"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+      tags                = { Name = "${local.name}-vpce-ssm" }
+    }
+    ssmmessages = {
+      service             = "ssmmessages"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+      tags                = { Name = "${local.name}-vpce-ssmmessages" }
+    }
+    ec2messages = {
+      service             = "ec2messages"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+      tags                = { Name = "${local.name}-vpce-ec2messages" }
+    }
+
+    secretsmanager = {
+      service             = "secretsmanager"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+      tags                = { Name = "${local.name}-vpce-secretsmanager" }
+    }
+  }
+}
